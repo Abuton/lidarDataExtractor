f175b6c0dab0b32ada0a7de58b3679bd94d5741d
Abuton <alaroabubakarolayemi@yahoo.com>
2021-08-24 21:04:39 +0100

    refactored and added 3d plotting function

--- lidardataextractor/visualizer/visualize.py
+++ lidardataextractor/visualizer/visualize.py
@@ -1,40 +1,85 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 import warnings
 warnings.filterwarnings('ignore')
-
 import earthpy.spatial as es
 import earthpy.plot as ep
 import rasterio as rio
-from shapely.geometry import box
 import geopandas as gpd
-from rasterio.transform import from_bounds, from_origin
-from rasterio.warp import reproject, Resampling
 
 class VisualizeRaster:
     """ 
     A module to visualize the raster files
     """
 
-    def __init__(self, data_file_path) -> None:
+    def __init__(self, data_file_path:str, shp_filepath:str) -> None:
         # Open the DEM with Rasterio
         with rio.open(data_file_path) as raster:
             elevation = raster.read(1, masked=True)
         self.elevation = elevation
+        self.gdf = gpd.read_file(shp_filepath)
+        self.gdf = self.gdf.set_crs(3857, allow_override=True)
+
+    def plot_geodataframe(self, color:str = 'whitesmoke', edgecolor:str = 'red') -> None:
+        """
+        Plots a Geodataframe with columns such as geometry and elevation
+
+        Parameters:
+        -----
+        color : color of the plotted image
+            (Default value = whitesmoke)
+        edgecolor : edgecolor of the plotted image
+            (Default value = red)
+
+        Returns:
+        --------
+        None
+        """
+        self.gdf.plot(figsize=(15,15), color=color, linestyle=':', edgecolor=edgecolor, aspect=1)
+        plt.show()
+
+    def plot_3D_visualzation(self, s: float = 0.01, color: str = "red"):
+        """
+        Plots a 3D terrain scatter plot of a geopandas dataframe
+
+        Parameters:
+
+            s (float, optional): S value. 
+                (Default value = 0.01)
+            color (str, optional): color of the points.
+                (Default value = red)
+
+        Returns:
+        --------
+        None
+        """
+
+        x = self.gdf.geometry.x
+        y = self.gdf.geometry.y
+        z = self.gdf.elevation
+
+        points = np.vstack((x, y, z)).transpose()
+
+        _, ax = plt.subplots(1, 1, figsize=(15, 15))
+        ax = plt.axes(projection='3d')
+        ax.scatter(points[:, 0], points[:, 1],
+                points[:, 2],  s=s, color=color)
+        plt.show()
+    
 
     def plot_bands(self, hillshade:bool=False, azimuth:int=150)->None:
         """
 
         Parameters
         ----------
-        hillshade:bool :
+        hillshade:bool : option to plot hillshade over raster image
              (Default value = False)
-        azimuth:int :
+        azimuth:int : The desired azimuth for the hillshade.
              (Default value = 150)
 
         Returns
         -------
+        None
 
         """
         if hillshade:
@@ -45,9 +90,9 @@ class VisualizeRaster:
                 hillshade,
                 cbar=False,
                 title="Hillshade made from DTM",
-                figsize=(10, 6),
+                figsize=(15, 15),
             )
-            plt.savefig('images/hillshade.png')
+            plt.savefig('hillshade.png')
             plt.show()
         else:
             # Plot the data
@@ -56,9 +101,9 @@ class VisualizeRaster:
                 cmap="gist_earth",
                 cbar=False,
                 title="DTM Without Hillshade",
-                figsize=(10, 6),
+                figsize=(15, 15),
             )
-            plt.savefig('images/DTM.png')
+            plt.savefig('DTM.png')
             plt.show()
                     
 
@@ -70,7 +115,7 @@ class VisualizeRaster:
         hillshade = es.hillshade(self.elevation)
 
         # Plot the DEM and hillshade at the same time
-        _, ax = plt.subplots(figsize=(10, 6))
+        _, ax = plt.subplots(figsize=(15, 15))
         ep.plot_bands(
             self.elevation,
             ax=ax,
@@ -80,98 +125,9 @@ class VisualizeRaster:
         )
         ax.imshow(hillshade, cmap="Greys", alpha=0.5)
         plt.show()
-        plt.savefig('images/overlay.png')
+        plt.savefig('overlay.png')
 
-    def explore(self, tif_path, product:str=None) -> None:
-        """
-
-        Parameters
-        ----------
-        tif_path : tif file path
-            
-        product:str :
-             (Default value = None)
-
-        Returns
-        -------
-
-        """
-        raster = rio.open(tif_path)
-        bounds = raster.bounds
-        west, south, east, north = bounds
-        src_crs = raster.crs
-        src_shape = src_height, src_width = raster.shape
-
-        # The src_transform is the Affine transformation for the source georeferenced raster.
-        # We calculate it using the from_bounds function provided by rasterio
-        src_transform = from_bounds(west, south, east, north, src_width, src_height)
-        source = raster.read(1)
-
-        dst_crs = {'init': 'EPSG:26915'}
-        dst_transform = from_origin(268000.0, 5207000.0, 250, 250)
-        dem_array = np.zeros((451, 623))
-        dem_array[:] = np.nan
-
-        reproject(source,
-            dem_array,
-            src_transform=src_transform,
-            src_crs=src_crs,
-            dst_transform=dst_transform,
-            dst_crs=dst_crs,
-            resampling=Resampling.bilinear)
-
-        topocmap = 'Spectral_r'
     
-        vmin = 180
-        vmax = 575
-        plt.figure(figsize=(10, 6))
-        ax = sns.displot(dem_array.ravel())
-        ax.set_xlabels('Elevation (m)')
-        ax = plt.gca()
-        ax.get_figure().savefig('images/1.png')
-
-        extent = xmin, xmax, ymin, ymax = 268000.0, 423500.0, 5094500.0, 5207000.0
-
-        def hillshade(array, azimuth, angle_altitude):
-            """
-
-            Parameters
-            ----------
-            array : raster array files
-                
-            azimuth :
-                
-            angle_altitude :
-                
-
-            Returns
-            -------
-
-            """
-
-            # Source: http://geoexamples.blogspot.com.br/2014/03/shaded-relief-images-using-gdal-python.html
-
-            x, y = np.gradient(array)
-            slope = np.pi/2. - np.arctan(np.sqrt(x*x + y*y))
-            aspect = np.arctan2(-x, y)
-            azimuthrad = azimuth*np.pi / 180.
-            altituderad = angle_altitude*np.pi / 180.
-
-
-            shaded = np.sin(altituderad) * np.sin(slope) \
-            + np.cos(altituderad) * np.cos(slope) \
-            * np.cos(azimuthrad - aspect)
-            return 255*(shaded + 1)/2
-
-        fig = plt.figure(figsize=(12, 8))
-        ax = fig.add_subplot(111)
-        ax.matshow(hillshade(dem_array, 30, 30),  cmap='Greys', alpha=.5, zorder=10)
-        cax = ax.contourf(dem_array, np.arange(vmin, vmax, 10),
-                        cmap=topocmap, vmin=vmin, vmax=vmax, origin='image')
-        fig.colorbar(cax, ax=ax)
-        fig.savefig('images/2.png')
-
-
 if __name__ == "__main__":
 
     dtm = "tif/iowa2.tif"
@@ -180,4 +136,3 @@ if __name__ == "__main__":
     viz.plot_bands(hillshade=True)
     viz.plot_bands(hillshade=False, azimuth=130)
     viz.plot_overlay()
-    viz.explore(tif_path='tif/SoPlatteRiver.tif')
